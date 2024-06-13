6f4a833f5073c8d336c704ab8a07ff4ba2abeca6
[radnoya] <radvanno2@gmail.com>
2024-06-13 21:14:19 +0200

    final

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,9 @@ problem-solving skills. It allows me to interact with a diverse range
 of customers, understand their issues, and provide timely solutions,
 enhancing my ability to manage time effectively and prioritize tasks</p>
 
-<b>2.Dig through logs to troubleshoot a customer's broken project</b>
+<b>2.Dig through logs to troubleshoot a customer&apos;s broken project</b>
 <p>Digging through logs requires analytical thinking and problem-solving skills.
-   It’s like being a detective, piecing together clues to find the root cause of an issue. 
+   It&apos;s like being a detective, piecing together clues to find the root cause of an issue. 
    Each log entry provides data points that require interpretation and analysis, keeping my mind engaged and challenged.</p>
 
 <b>3.Help Train and Onboard New Support Teammates</b>
@@ -43,18 +43,18 @@ enhancing my ability to manage time effectively and prioritize tasks</p>
 <h1>Ranking my least favourite support tasks</h1>
 <br />
 <b>1.Scheduling time-off coverage and collaborating as part of a growing cohesive support team</b>
-<p>While I don't have prior experience in scheduling time-off coverage, I'm genuinely eager and open to learning.
-   I believe with guidance and support, I can quickly grasp the intricacies of this task and contribute effectively to the team's success.</p>
+<p>While I don&apos;t have prior experience in scheduling time-off coverage, I&apos;m genuinely eager and open to learning.
+   I believe with guidance and support, I can quickly grasp the intricacies of this task and contribute effectively to the team&apos;s success.</p>
 
 <b>2.Run ticket review sessions to make sure tone is consistent</b>
-<p>I want to express that while I haven't had the opportunity to lead ticket review sessions before,
-   I'm keen to learn and apply myself to this task. </p>
+<p>I want to express that while I haven&apos;t had the opportunity to lead ticket review sessions before,
+   I&apos;m keen to learn and apply myself to this task. </p>
 
 <b>3.Find and recruit teammates for the support team</b>
 <p>While I recognize the importance of finding and recruiting teammates for the support team,
    I believe that this task is best handled by HR professionals who specialize in talent acquisition.
-  I feel that focusing on tasks aligned with my strengths would allow me to contribute most effectively to the team's objectives.
-  I trust in the HR team's expertise to source and select candidates who are the best fit for our team, enabling us to maintain a high standard of support and collaboration.</p>
+  I feel that focusing on tasks aligned with my strengths would allow me to contribute most effectively to the team&apos;s objectives.
+  I trust in the HR team&apos;s expertise to source and select candidates who are the best fit for our team, enabling us to maintain a high standard of support and collaboration.</p>
 
 <b>4.Manage a support team</b>
 <p>While I appreciate the importance of effective team management within the support department,
@@ -64,7 +64,7 @@ enhancing my ability to manage time effectively and prioritize tasks</p>
 
 <b>5.Analyze hundreds of support tickets to spot trends the product team can use</b>
 <p>I believe that leveraging machine learning tools and automated programs could streamline this process more effectively.
-   Focus should be more on optimizing these tools to ensure we're utilizing them to their fullest potential.</p>
+   Focus should be more on optimizing these tools to ensure we&apos;re utilizing them to their fullest potential.</p>
 
 <br />
 <br />
@@ -74,7 +74,7 @@ enhancing my ability to manage time effectively and prioritize tasks</p>
 <br />
 
 <ul>
-  <li>I am eager to deepen my knowledge of cloud technologies especially within the context of Vercel's platform. </li>
+  <li>I am eager to deepen my knowledge of cloud technologies especially within the context of Vercel&apos;s platform. </li>
   <li>AI</li>
   <li>Additionally, I am interested in learning more about customer success strategies to better align product features with customer needs and drive higher satisfaction and retention rates.
   </li>
@@ -87,8 +87,8 @@ enhancing my ability to manage time effectively and prioritize tasks</p>
 <p>As mentioned previously, I am/was the main point of contact of billing tickets in my current role. 
   A customer reached out to us with a mostly simple flat renewal of their subscription. I was dealing with the request as usual,
   following our usual guidlines, while the billing engineering team was implementing new changes in the process. 
-  The customer's new invoice failed in the system, which meant that their subscription straight away was cancelled. 
-  This situation required prompt response from the whole team's end to put back on track this customer. Fortunately we couldoffer them
+  The customer&apos;s new invoice failed in the system, which meant that their subscription straight away was cancelled. 
+  This situation required prompt response from the whole team&apos;s end to put back on track this customer. Fortunately we couldoffer them
   an even better subscription so they left with smile on their face.
 </p>
 <br />
@@ -111,9 +111,9 @@ enhancing my ability to manage time effectively and prioritize tasks</p>
   <li>Request the specific build commands and environment configurations they are using.</li>
   <li>Inquire about recent changes to their code or configuration that might have triggered the issue.</li>
   <br />
-  <li>Look up the framework’s documentation to understand its build process.</li>
+  <li>Look up the framework&apos;s documentation to understand its build process.</li>
   <li>Search for common issues or similar error messages in forums and support communities.</li>
-  <li>Review Vercel's own documentation and any related support tickets.</li>
+  <li>Review Vercel&apos;s own documentation and any related support tickets.</li>
   <br />
   <li>Try to reproduce the issue in a controlled environment to understand its scope and identify potential causes.</li>
 </ul>
@@ -145,7 +145,7 @@ Noemi
       <br />
       <p>
       Dear Customer,
-To redirect the /blog path to https://example.com, you can add a redirect rule in your vercel.json configuration file. Here’s how you can do it:
+To redirect the /blog path to https://example.com, you can add a redirect rule in your vercel.json configuration file. Here&apos;s how you can do it:
     1. Open your vercel.json file.
     2. Add the following redirects configuration:
 json
@@ -155,13 +155,13 @@ This configuration will create a permanent redirect from /blog to https://exampl
 If you have any other questions or need further assistance, feel free to reach out.
 Kinds,
 Noemi
-Decision Making Process: I chose this method because it leverages Vercel’s native redirect functionality, ensuring efficient and server-side handling of the redirect with minimal impact on performance.
+Decision Making Process: I chose this method because it leverages Vercel&apos;s native redirect functionality, ensuring efficient and server-side handling of the redirect with minimal impact on performance.
       </p>
 <br />
 <br />
 <br />
 <b>Preventing Indexing</b>
-<p>To prevent your project from being indexed by search engines, you can add a robots.txt file to your project’s root directory with the following content:
+<p>To prevent your project from being indexed by search engines, you can add a robots.txt file to your project&apos;s root directory with the following content:
 makefile
 User-agent: *
 Disallow: /
